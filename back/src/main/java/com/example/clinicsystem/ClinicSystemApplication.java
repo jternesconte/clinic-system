803329f86591a7ddd33965dcace3b2bbf0bef335
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2024-10-29 20:50:31 -0300

    first commit

---
+++ back/src/main/java/com/example/clinicsystem/ClinicSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.clinicsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicSystemApplication.class, args);
+	}
+
+}
