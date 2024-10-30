b3d49547e78fdea5f68d77db0e27ec6f7ecdcdd4
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2024-10-30 19:50:50 -0300

    feat: add Patient Entity

---
+++ back/src/main/java/com/example/clinicsystem/models/entities/Patient.java
@@ -0,0 +1,89 @@
+package com.example.clinicsystem.models.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "patient")
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(length = 100, nullable = false)
+    private String name;
+
+    @Column(name = "birth_date", nullable = false)
+    private Date birthDate;
+
+    @Column(length = 15, nullable = false)
+    private String phone;
+
+    @Column(length = 100, nullable = false)
+    private String email;
+
+    @Column(length = 255, nullable = false)
+    private String address;
+
+    public Patient() {
+
+    }
+
+    public Patient(String name, Date birthDate, String phone, String email, String address) {
+        this.name = name;
+        this.birthDate = birthDate;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
