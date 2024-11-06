a3d94c877f3c3a3f5fa3d1ebeb2d5ea5bfa1e6cf
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2024-11-05 21:09:10 -0300

    feat: add requests for getAll, create and edit patients

--- back/src/main/java/com/example/clinicsystem/ws/PatientWs.java
+++ back/src/main/java/com/example/clinicsystem/ws/PatientWs.java
@@ -1,9 +1,11 @@
 package com.example.clinicsystem.ws;
 
 import com.example.clinicsystem.controllers.PatientController;
+import com.example.clinicsystem.models.entities.Patient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/patient")
@@ -11,4 +13,19 @@ public class PatientWs {
 
     @Autowired
     private PatientController patientController;
+
+    @GetMapping("/getAll")
+    public @ResponseBody List<Patient> getAllPatients() {
+        return patientController.getPatients();
+    }
+
+    @PostMapping("/create")
+    public Patient newPatient(@RequestBody Patient patientDetails) {
+        return patientController.createPatient(patientDetails);
+    }
+
+    @PutMapping("/edit/{id}")
+    public Patient editPatient(@PathVariable int id, @RequestBody Patient patientDetails) {
+        return patientController.alterPatient(id, patientDetails);
+    }
 }
