e4f54aa700c454e9f435fc1176ad953b0cdf1f77
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2024-11-06 21:20:21 -0300

    feat: add requests for add, edit and getAll doctors

--- back/src/main/java/com/example/clinicsystem/ws/DoctorWs.java
+++ back/src/main/java/com/example/clinicsystem/ws/DoctorWs.java
@@ -1,9 +1,11 @@
 package com.example.clinicsystem.ws;
 
 import com.example.clinicsystem.controllers.DoctorController;
+import com.example.clinicsystem.models.entities.Doctor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/doctor")
@@ -11,4 +13,19 @@ public class DoctorWs {
 
     @Autowired
     DoctorController doctorController;
+
+    @GetMapping("/getAll")
+    public @ResponseBody List<Doctor> getAllDoctors() {
+        return doctorController.getDoctors();
+    }
+
+    @PostMapping("/create")
+    public Doctor newDoctor(@RequestBody Doctor doctorDetails) {
+        return doctorController.createDoctor(doctorDetails);
+    }
+
+    @PutMapping("/edit/{id}")
+        public Doctor editDoctor(@PathVariable int id, @RequestBody Doctor doctorDetails) {
+        return doctorController.alterDoctor(id, doctorDetails);
+    }
 }
