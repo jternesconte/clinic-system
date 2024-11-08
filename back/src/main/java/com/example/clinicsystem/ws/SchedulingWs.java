fdd0d3ab44c9d7bb509131ad8cee8ee3609c7b0d
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2024-11-07 21:36:31 -0300

    feat: add requests for add, edit and getAll schedulings

--- back/src/main/java/com/example/clinicsystem/ws/SchedulingWs.java
+++ back/src/main/java/com/example/clinicsystem/ws/SchedulingWs.java
@@ -1,9 +1,11 @@
 package com.example.clinicsystem.ws;
 
 import com.example.clinicsystem.controllers.SchedulingController;
+import com.example.clinicsystem.models.entities.Scheduling;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/scheduling")
@@ -11,4 +13,19 @@ public class SchedulingWs {
 
     @Autowired
     SchedulingController schedulingController;
+
+    @GetMapping("/getAll")
+    public @ResponseBody List<Scheduling> getAllSchedulings() {
+        return schedulingController.getSchedulings();
+    }
+
+    @PostMapping("/create/{idPatient}/{idDoctor}")
+    public Scheduling newScheduling(@PathVariable int idPatient, @PathVariable int idDoctor, @RequestBody Scheduling schedulingDetails) {
+        return schedulingController.createScheduling(idPatient, idDoctor, schedulingDetails);
+    }
+
+    @PutMapping("edit/{id}")
+    public Scheduling editScheduling(@PathVariable int id, @RequestBody Scheduling schedulingDetails) {
+        return schedulingController.alterScheduling(id, schedulingDetails);
+    }
 }
